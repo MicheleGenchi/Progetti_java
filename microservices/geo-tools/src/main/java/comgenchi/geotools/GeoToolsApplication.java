dbf504b2449221d022280761295e3970b9c7a64e
MicheleGenchi <cell.mgenchi@gmail.com>
2026-09-26 14:04:22 +0200

    corsConfigurer per le chiamate esterne

--- microservices/geo-tools/src/main/java/comgenchi/geotools/GeoToolsApplication.java
+++ microservices/geo-tools/src/main/java/comgenchi/geotools/GeoToolsApplication.java
@@ -2,6 +2,9 @@ package comgenchi.geotools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GeoToolsApplication {
@@ -10,4 +13,18 @@ public class GeoToolsApplication {
 		SpringApplication.run(GeoToolsApplication.class, args);
 	}
 
+	    // Incolla questo blocco qui sotto:
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://0.0.0.0:7001","http://localhost:7001","http://0.0.0.0:8081")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
